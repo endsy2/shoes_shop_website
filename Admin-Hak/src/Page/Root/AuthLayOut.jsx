c815c93b2b08632a21857643763c35ef3a137190
ChanSenghak <163015944+ChanSenghak@users.noreply.github.com>
2025-03-27 23:11:44 +0700

    go

--- Admin-Hak/src/Page/Root/AuthLayOut.jsx
+++ Admin-Hak/src/Page/Root/AuthLayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import {letter2} from "../../assets/shoes image/letter2.svg";
 import React from "react";
 const AuthLayOut = () => {
   return (
     <div>
       <h1>Auth Layout</h1>
+      <img src={letter2} alt="" className="p-16" />
       <Outlet /> {/* This will render child routes inside AuthLayOut */}
     </div>
   );
--- Admin-Section/src/Utils/Product/InventoryOperation.js
+++
@@ -1,8 +0,0 @@
-import { Inventory_items } from "../../Constants";
-
-export const Inventory_sum = () => {
-  return Inventory_items.reduce(
-    (acc, item) => acc + parseInt(item.inventory),
-    0,
-  );
-};
